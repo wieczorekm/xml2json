f2f366a8efdbb7eefd71ff504a32cbb3bf622235
wieczorekm <wmarch112@gmail.com>
2018-01-09 17:59:58 +0100

    use parser in main file

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from lexer import *
+from lexer import Lexer
+from parser import Parser
 import sys
+
 
 if len(sys.argv) == 2:
     f = open(sys.argv[1], 'r')
     content = f.read()
 
     lexer = Lexer(content)
-    token = lexer.get_next_token()
-    while not isinstance(token, EndOfTextToken):
-        print(token)
-        token = lexer.get_next_token()
-    print(token)
+    parser = Parser(lexer)
+
+    document_tree = parser.get_document_tree()
+    pass
 
 
 else:
