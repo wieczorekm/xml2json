e26cb6773c0d95ccc443bdab4397a205bedfa9e0
wieczorekm <wmarch112@gmail.com>
2018-01-21 13:28:56 +0100

    indentation fixups

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from src.lexer import Lexer
 from src.parser import Parser
 import sys
 
+from src.translator import Translator
 
 if len(sys.argv) == 2:
     f = open(sys.argv[1], 'r')
@@ -9,9 +10,14 @@ if len(sys.argv) == 2:
 
     lexer = Lexer(content)
     parser = Parser(lexer)
-
     document_tree = parser.get_document_tree()
-    pass
+
+    translator = Translator(document_tree)
+    json = translator.get_json()
+
+    output_file = open("output.json", "w")
+    output_file.write(json)
+    output_file.close()
 
 
 else:
--- src/translator.py
+++ src/translator.py
@@ -1,4 +1,3 @@
-
 class Translator:
     def __init__(self, document_tree):
         self.document_tree = document_tree
@@ -8,33 +7,41 @@ class Translator:
         prolog = self.document_tree.prolog
         json += self.translate_prolog(prolog) if prolog else ""
         xml = self.document_tree.xml
-        json += '\t"' + xml.tag + '":' + self.translate_xml(xml)
-        json += '}'
+        json += '\t"' + xml.tag + '":' + self.translate_xml(xml, 2)
+        json += '\n}'
         return json
 
-    def translate_xml(self, xml):
+    def translate_xml(self, xml, nest_level):
         if len(xml.xmls) > 0:
             inner = '{\n'
             for idx, loop_xml in enumerate(xml.xmls):
-                inner += '\t"' + loop_xml.tag + '": ' + self.translate_xml(loop_xml)
+                inner += "\t" * nest_level
+                inner += '"' + loop_xml.tag + '": ' + self.translate_xml(loop_xml, nest_level + 1)
                 inner += '\n' if idx == len(xml.xmls) - 1 else ',\n'
-            inner += '}\n'
+            inner += "\t" * (nest_level - 1)
+            inner += '}'
             return inner
         else:
-            return '"' + xml.value + '"\n' if not xml.attributes else self.translate_body_with_attributes(xml)
+            return self.get_xml_value(xml.value) if not xml.attributes else self.translate_body_with_attributes(xml, nest_level)
 
-    def translate_body_with_attributes(self, xml):
+    def translate_body_with_attributes(self, xml, nest_level):
         body = '{\n'
         for attr in sorted(xml.attributes):
-            body += '\t"attr-' + attr + '": "' + xml.attributes[attr] + '",\n'
-        body += '"#text": "' + xml.value + '"\n'
-        body += '}\n'
+            body += "\t" * nest_level
+            body += '"attr-' + attr + '": "' + xml.attributes[attr] + '",\n'
+        body += "\t" * nest_level
+        body += '"#text":' + self.get_xml_value(xml.value) + '\n'
+        body += "\t" * (nest_level - 1)
+        body += '}'
         return body
 
     def translate_prolog(self, prolog):
-        prolog_as_string = '"prolog":{\n'
+        prolog_as_string = '\t"prolog":{\n'
         for idx, attr in enumerate(sorted(prolog.attributes)):
-            prolog_as_string += '"' + attr + '": "'+ prolog.attributes[attr]
-            prolog_as_string += '"\n' if idx == len(prolog.attributes) - 1 else '",\n'
-        prolog_as_string += "},\n"
+            prolog_as_string += '\t\t"' + attr + '": "' + prolog.attributes[attr]
+            prolog_as_string += '"\n' if idx == len(prolog.attributes) - 1 else '\",\n'
+        prolog_as_string += "\t},\n"
         return prolog_as_string
+
+    def get_xml_value(self, value):
+        return '"' + value + '"' if value else "null"
